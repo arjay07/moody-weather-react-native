5e5c2474326a9a03053767e745fc99ebb7013214
leandroyabut <leandro.yabut@smoothstack.com>
2021-11-03 22:19:08 -0600

    Add environment variables to project.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { API_URL } from '@env';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is the API URL I'm using: {API_URL}</Text>
       <StatusBar style="auto" />
     </View>
   );
